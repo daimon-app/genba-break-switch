39311906fe660738a53b3d66dc31392cf473d4bc
daimon-app <teppei.tn.nt@gmail.com>
2026-06-13 23:12:45 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+// DAIMON Service Worker
+// オフラインでも開けるようにキャッシュする
+
+const CACHE_NAME = 'daimon-v5';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './assets/sounds/ocean-waves.mp3',
+  './assets/sounds/river.mp3',
+  './assets/sounds/forest.mp3',
+  './assets/sounds/rain.mp3',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).catch(() => cached);
+    })
+  );
+});
